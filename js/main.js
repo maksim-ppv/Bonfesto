6761a492fbeb06289f7524d3e22fe2f57bca1b40
maksim-ppv <popovm1502@gmail.com>
2020-03-27 22:08:35 +0300

    Правки.

--- js/main.js
+++ js/main.js
@@ -2,12 +2,16 @@ $(document).ready(function(){
     $('.content-slider').slick({
         arrows: false,
         fade: true,
+        waitForAnimate: false,
+        initialSlide: 1,
         slidesToShow: 1, 
         slidesToScroll: 1,
         asNavFor: '.links',
         speed: 500
     });
     $('.links').slick({
+        variableWidth: true,
+        initialSlide: 1,
         slidesToShow: 4,
         asNavFor: '.content-slider',
         focusOnSelect: true
